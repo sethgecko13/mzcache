d9fd69f2509c720e95f113d9d379b9f81ff0dbad
Nicholas Rees <nicholasrees969@gmail.com>
2024-12-09 20:06:11 -0500

    initial commit

---
+++ cache.go
@@ -0,0 +1,161 @@
+package mzcache
+
+import (
+	"compress/gzip"
+	"crypto/sha256"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/gofrs/flock"
+)
+
+var LockPath = getLockPath()
+var cachePath = "/var/tmp/mzcache"
+
+var ErrCacheEmptyString = errors.New("empty string passed in to cache")
+var ErrCacheCreateDirectory = errors.New("unable to create cache directory")
+var ErrCacheDirectoryNotExist = errors.New("cache directory does not exist")
+var ErrCacheCreate = errors.New("unable to create cache file")
+var ErrCacheWrite = errors.New("unable to write to cache file")
+var ErrCacheSync = errors.New("unable to sync cache file to filesystem")
+
+var ErrCacheLock = errors.New("unable to lock cache file")
+var ErrCacheUnlock = errors.New("unable to unlock cache file")
+
+var ErrCacheMiss = errors.New("cache file does not exist")
+var ErrCacheDecompress = errors.New("unable to decompress cache file")
+var ErrCacheRead = errors.New("unable to read cache file")
+
+// all the other errors we can use errors.Join to give details about the error.
+// with expired cache, we have to handle separately because there is no underlying error.
+type ErrCacheExpired struct {
+	FullPath string
+}
+
+func (e *ErrCacheExpired) Error() string {
+	return fmt.Sprintf("cache expired %s", e.FullPath)
+}
+
+// since caching is so fundamental to my app, I choose to panic if caching does not work.
+// you may want to make different decisions if you use this library.
+func getLockPath() string {
+	err := os.RemoveAll("/tmp/mz*")
+	if err != nil {
+		panic("unable to remove previous lock files")
+	}
+	dname, err := os.MkdirTemp("", "mz")
+	if err != nil {
+		panic("unable to create cache lock file")
+	}
+	return dname
+}
+func hash(key string) string {
+	h := sha256.New()
+	h.Write([]byte(key))
+	bs := h.Sum(nil)
+	result := fmt.Sprintf("%x", bs)
+	return result
+}
+func getCacheFilePath(key string) (path string, fullPath string, hashKey string) {
+	hashKey = fmt.Sprintf("%v", hash(key))
+	path = fmt.Sprintf("%v/%v/%v/", cachePath, hashKey[0:2], hashKey[2:4])
+	fullPath = fmt.Sprintf("%v%v.gz", path, hashKey[4:64])
+	return path, fullPath, hashKey
+}
+func getFileLockPath(hashKey string) string {
+	return fmt.Sprintf("%s/%s", LockPath, hashKey)
+}
+func WriteCache(key string, value string) error {
+	// don't write empty values they are probably errors
+	if value == "" {
+		return ErrCacheEmptyString
+	}
+	path, fullPath, hashKey := getCacheFilePath(key)
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		err := os.MkdirAll(path, 0750)
+		if err != nil {
+			return errors.Join(ErrCacheCreateDirectory, err)
+		}
+	}
+	fileLock := flock.New(getFileLockPath(hashKey))
+	err := fileLock.Lock()
+	if err != nil {
+		return errors.Join(ErrCacheLock, err)
+	}
+	var lockError error
+	defer func() {
+		err = fileLock.Unlock()
+		if err != nil {
+			lockError = errors.Join(ErrCacheUnlock, err)
+		}
+	}()
+	// no need to remove file, this causes race conditions, create removes previous contents
+	file, err := os.Create(filepath.Clean(fullPath))
+	if err != nil {
+		return errors.Join(ErrCacheCreate, err)
+	}
+	defer file.Close()
+	g := gzip.NewWriter(file)
+	defer g.Close()
+	_, err = g.Write([]byte(value))
+	if err != nil {
+		return errors.Join(ErrCacheWrite, err)
+	}
+	err = file.Sync()
+	if err != nil {
+		return errors.Join(ErrCacheSync, err)
+	}
+	return lockError
+}
+func GetCache(key string, days int) (string, error) {
+	var result string
+	path, fullPath, hashKey := getCacheFilePath(key)
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		return result, errors.Join(ErrCacheDirectoryNotExist, err)
+	}
+	fileLock := flock.New(getFileLockPath(hashKey))
+	err := fileLock.Lock()
+	if err != nil {
+		return result, errors.Join(ErrCacheLock, err)
+	}
+	var lockError error
+	defer func() {
+		err = fileLock.Unlock()
+		if err != nil {
+			lockError = errors.Join(ErrCacheUnlock, err)
+		}
+	}()
+	if _, err := os.Stat(fullPath); os.IsNotExist(err) {
+		return result, errors.Join(ErrCacheMiss, err)
+	} else {
+		file, _ := os.Stat(fullPath)
+		now := time.Now()
+		dt := now.AddDate(0, 0, ((days - 1) * -1))
+		cutoff := dt.Format("2006-01-02")
+		fileDate := file.ModTime()
+		fd := fileDate.Format("2006-01-02")
+		if fd < cutoff {
+			return result, &ErrCacheExpired{FullPath: fullPath}
+		}
+		inputFile, err := os.Open(filepath.Clean(fullPath))
+		if err != nil {
+			return result, errors.Join(ErrCacheRead, err)
+		}
+		defer inputFile.Close()
+		reader, err := gzip.NewReader(inputFile)
+		if err != nil {
+			return result, errors.Join(ErrCacheDecompress, err)
+		}
+		defer reader.Close()
+		content, err := io.ReadAll(reader)
+		if err != nil {
+			return result, errors.Join(ErrCacheRead, err)
+		}
+		result = string(content)
+		return result, lockError
+	}
+}
---
+++ cache_test.go
@@ -0,0 +1,124 @@
+package mzcache
+
+import (
+	"errors"
+	"fmt"
+	"sync"
+	"testing"
+)
+
+func testWrite() error {
+	expected := "value1\nvalue2\nvalue3"
+	key := "test1"
+	err := WriteCache(key, expected)
+	if err != nil {
+		return err
+	}
+	result, _ := GetCache(key, 1)
+	if result != expected {
+		return fmt.Errorf("result was incorrect, got: %v, want: %v.", result, expected)
+	}
+	return nil
+}
+
+func TestWriteCache(t *testing.T) {
+	t.Parallel()
+	err := testWrite()
+	if err != nil {
+		t.Errorf("got an unexpected error: %v", err.Error())
+	}
+}
+func TestWriteCacheEmptyString(t *testing.T) {
+	t.Parallel()
+	value := ""
+	key := "empty"
+	err := WriteCache(key, value)
+	if err != ErrCacheEmptyString {
+		t.Errorf("got %v, expected: %v", err.Error(), ErrCacheEmptyString.Error())
+	}
+}
+func TestGetCacheExpired(t *testing.T) {
+	t.Parallel()
+	expected := "value1\nvalue2\nvalue3"
+	key := "expired"
+	err := WriteCache(key, expected)
+	if err != nil {
+		t.Errorf("got unexpected error: %v", err.Error())
+	}
+	_, err = GetCache(key, -1)
+	var expiredErr *ErrCacheExpired
+	if !errors.As(err, &expiredErr) {
+		t.Errorf("got unexpected error: %v", err.Error())
+	}
+}
+func TestGetCacheHit(t *testing.T) {
+	t.Parallel()
+	expected := "value1\nvalue2\nvalue3"
+	key := "test2"
+	err := WriteCache(key, expected)
+	if err != nil {
+		t.Errorf("got unexpected error: %v", err.Error())
+	}
+	result, err := GetCache(key, 1)
+	if err != nil {
+		t.Errorf("got unexpected error: %v", err.Error())
+	}
+	if result != expected {
+		t.Errorf("result was incorrect, got: %v, want: %v.", result, expected)
+	}
+}
+func TestGetCacheInvalidDirectory(t *testing.T) {
+	// Don't run in parallel, will break other tests
+	oldCachePath := cachePath
+	cachePath = "/var/tmp/blah"
+	defer func() {
+		cachePath = oldCachePath
+	}()
+	_, err := GetCache("invalid_directory", 1)
+	if !errors.Is(err, ErrCacheDirectoryNotExist) {
+		t.Errorf("result was incorrect, got: %v, want: %v.", err, ErrCacheDirectoryNotExist.Error())
+	}
+}
+func TestGetCacheMiss(t *testing.T) {
+	t.Parallel()
+
+	_, err := GetCache("test3", 1)
+	if !errors.Is(err, ErrCacheMiss) {
+		t.Errorf("Cache existed but shouldn't have")
+	}
+}
+func TestGetCacheFilePath(t *testing.T) {
+	t.Parallel()
+	path, file, hashKey := getCacheFilePath("testpath")
+	if path != cachePath+"/fd/4f/" {
+		t.Errorf("Cache path is wrong, %v", path)
+	}
+	if file != cachePath+"/fd/4f/62f64cf7e327dc5a460e1c3ab20b097365438a74977da31d3e93b2299247.gz" {
+		t.Errorf("Cache file is wrong, %v", file)
+	}
+	if hashKey != "fd4f62f64cf7e327dc5a460e1c3ab20b097365438a74977da31d3e93b2299247" {
+		t.Errorf("hashKey is wrong, %v", hashKey)
+	}
+}
+func TestWriteUnderLoad(t *testing.T) {
+	t.Parallel()
+	errors := []error{}
+	var wg sync.WaitGroup
+	var errMutex sync.Mutex
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			err := testWrite()
+			if err != nil {
+				errMutex.Lock()
+				errors = append(errors, err)
+				errMutex.Unlock()
+			}
+		}()
+	}
+	wg.Wait()
+	if len(errors) != 0 {
+		t.Errorf("got unexpected errors, %v", errors)
+	}
+}
